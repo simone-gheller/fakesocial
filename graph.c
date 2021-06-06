1dbb2b868a3cacdc33cbee00723396a977e4ca66
Simone Gheller <simone.gheller1@gmail.com>
2021-06-06 22:20:51 +0200

    added relationship and report features

--- graph.c
+++ graph.c
@@ -32,3 +32,37 @@ void addrel(Hashtable* t, _Bool** matrix, char* receiver, char* giver){
     if(rec_id != -1 && giv_id != -1)
         matrix[rec_id][giv_id] = 1;
 }
+
+
+void delrel(Hashtable t*, _Bool** matrix, char* receiver, char* giver){
+    int rec_id = getIdFromHash(t,receiver);
+    int giv_id = getIdFromHash(t, giver);
+    if(rec_id != -1 && giv_id != -1)
+        matrix[rec_id][giv_id] = 0;
+}
+
+void del_entity(Hashtable t*, _Bool** matrix, char* entity ){
+    int id = getIdFromHash(t, entity);
+    for(int i=0;i<COLUMNS;i++){
+        matrix[id][i] = 0;
+    }
+    for(int i=0;i<ROWS;i++){
+        matrix[i][id] = 0;
+    }
+    delete(t,entity)
+}
+
+
+void report(_Bool* matrix){
+    int max=0;
+    for(int i=0;i<ROWS){
+        int counter=0
+        for(int j=0;j<COLUMNS;j++){
+            counter++;
+        }
+        if(counter>max){
+            max=counter;
+        }
+    }
+    printf("%d",max);
+}
